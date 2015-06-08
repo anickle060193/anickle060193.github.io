96a2bd8d1261e6292bfcdfa19a61c49fec048243
anickle060193 <anickle060193@gmail.com>
2015-06-08 14:28:31 -0500

    Fixed issue with negative elapsed time values in animate.

--- js/utilities.js
+++ js/utilities.js
@@ -86,7 +86,7 @@ function animate( time )
     var elapsedTime = time - animator.lastTime;
     animator.lastTime = time;
     animator.renderer();
-    if( elapsedTime < 100 )
+    if( 0 < elapsedTime && elapsedTime < 100 )
     {
         animator.updater( elapsedTime / 1000 );
     }
