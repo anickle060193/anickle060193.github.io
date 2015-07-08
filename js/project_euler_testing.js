91fb903109142dd5e03ccd7fb8c0ac2108f8b40e
Adam Nickle <anickle060193@gmail.com>
2015-07-08 16:31:30 -0500

    Working on Project Euler problem.

--- js/project_euler_testing.js
+++ js/project_euler_testing.js
@@ -1,5 +1,48 @@
-if( true )
+if( false )
 {
     var div = document.getElementById( "testingPanel" );
     div.parentNode.removeChild( div );
+}
+
+function isPalindrome( num )
+{
+    var str = num.toString();
+    var len = str.length;
+    for( var i = 0; i < len / 2; i++ )
+    {
+        if( str[ i ] !== str[ len - i - 1 ] )
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+function reverse( num )
+{
+    var str = num.toString();
+    var len = str.length;
+    for( var i = 0; i < len / 2; i++ )
+    {
+        var temp = str[ i ];
+        str[ i ] = str[ len - i - 1 ];
+        str[ len - i - 1 ] = temp;
+        console.log( str );
+    }
+    return Number( str );
+}
+
+var maxIterations = 50;
+function isLychrel( num )
+{
+    for( var i = 1; i <= maxIterations; i++ )
+    {
+
+        num += reverse( num );
+        if( isPalindrome( num ) )
+        {
+            return true;
+        }
+    }
+    return false;
 }
