69149731d8015c45cc7b1f2572649eff200b3dae
Adam Nickle <anickle060193@gmail.com>
2015-07-09 13:38:10 -0500

    Working on Project Euler problem 58.

--- js/project_euler_testing.js
+++ js/project_euler_testing.js
@@ -4,45 +4,114 @@ if( false )
     div.parentNode.removeChild( div );
 }
 
-function isPalindrome( num )
+function isPrime( num )
 {
-    var str = num.toString();
-    var len = str.length;
-    for( var i = 0; i < len / 2; i++ )
+    var sqrt = Math.sqrt( num );
+    for( var i = 2; i <= sqrt; i++ )
     {
-        if( str[ i ] !== str[ len - i - 1 ] )
+        if( num % i === 0 )
         {
             return false;
         }
     }
-    return true;
+    return num !== 1;
 }
 
-function reverse( num )
-{
-    var str = num.toString();
-    var len = str.length;
-    for( var i = 0; i < len / 2; i++ )
-    {
-        var temp = str[ i ];
-        str[ i ] = str[ len - i - 1 ];
-        str[ len - i - 1 ] = temp;
-        console.log( str );
-    }
-    return Number( str );
-}
+var state_right = new State( 0, 1 );
+var state_up = new State( -1, 0 );
+var state_left = new State( 0, -1 );
+var state_down = new State( 1, 0 );
 
-var maxIterations = 50;
-function isLychrel( num )
-{
-    for( var i = 1; i <= maxIterations; i++ )
-    {
+state_right.next = state_up;
+state_up.next = state_left;
+state_left.next = state_down;
+state_down.next = state_right;
 
-        num += reverse( num );
-        if( isPalindrome( num ) )
+function State( vInc, hInc )
+{
+    this.increment = { v: vInc, h: hInc };
+    this.next = null;
+
+    this.updateStep = function( grid, row, col )
+    {
+        var r = row + this.next.increment.v;
+        var c = col + this.next.increment.h;
+        if( grid.get( r, c ) === invalid )
         {
-            return true;
+            return this.next;
         }
+        else
+        {
+            return this;
+        }
+    };
+}
+
+var invalid = -1;
+
+function Grid()
+{
+    this.rings = 1;
+
+    this._offset = Math.floor( Math.pow( 2, 31 ) );
+    this._grid = [ ];
+    this._r = 0;
+    this._c = 1;
+    this._state = state_up;
+    this._pow = 1;
+
+    this.set( 0, 0, 1 );
+}
+Grid.prototype.get = function( row, col )
+{
+    row += this._offset;
+    col += this._offset;
+    if( this._grid[ row ] === undefined )
+    {
+        return -1;
     }
-    return false;
+    else if( this._grid[ row ][ col ] === undefined )
+    {
+        return -1;
+    }
+    else
+    {
+        return this._grid[ row ][ col ];
+    }
+};
+Grid.prototype.set = function( row, col, value )
+{
+    row += this._offset;
+    col += this._offset;
+    if( this._grid[ row ] === undefined )
+    {
+        this._grid[ row ] = [ ];
+    }
+    this._grid[ row ][ col ] = value;
+};
+Grid.prototype.addRing = function()
+{
+    var i = this._pow * this._pow + 1;
+    this._pow += 2;
+    var max = this._pow * this._pow;
+    while( i <= max )
+    {
+        this.set( this._r, this._c, i );
+        this._r += this._state.increment.v;
+        this._c += this._state.increment.h;
+
+        this._state = this._state.updateStep( this, this._r, this._c );
+        i++;
+    }
+    this.rings++;
+};
+
+var grid = new Grid();
+
+function getPercentage()
+{
+    for( var i = grid.rings; i++ )
+    {
+
+    }
 }
